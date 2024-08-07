12dc47c98dca20d80b1ea9729f435f85869ec10f
titopotito <jdf.tito@gmail.com>
2024-08-07 11:28:50 +0800

    Added temp UserListView.

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
+from .serializer import UserSerializer
 
 
-class UserListView(generics.ListAPIView):
+class UserView(generics.ListAPIView):
     queryset = User.objects.all()
+    serializer_class = UserSerializer
